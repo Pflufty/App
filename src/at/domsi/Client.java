2008cbe91625c9da596e74100e74f8f690e5d496
Kirchsteiger Dominik <domsi198@gmail.com>
2016-06-20 00:00:50 +0200

    v2 Schere,Stein,Papier funktionierend

--- src/at/domsi/Client.java
+++ src/at/domsi/Client.java
@@ -5,32 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
 	public static void main(String[] args) {
+		
 		try {
-			Socket client = new Socket("127.0.0.1", 9812);
-
+			Socket client = new Socket("localhost", 9812);
+			
+			PrintWriter outStream = new PrintWriter(client.getOutputStream(), true);
+			BufferedReader inStream = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			
+			System.out.println(inStream.readLine());
+			
+			while(inStream.readLine().equals("Wait")){
+				
+			}
+			
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Gib eine Nummer ein:");
-			int nr = sc.nextInt();
-			sc.close();
+			int input;
 			
-			PrintWriter inStream =
-                    new PrintWriter(client.getOutputStream(), true);
-			inStream.println(nr);
+			System.out.println("Gib eine Nummer ein<1,2,3>:");
+			input = sc.nextInt();
+			outStream.println(input);
 			
-			BufferedReader outStream =
-		            new BufferedReader(new InputStreamReader(client.getInputStream()));
+			while(inStream.readLine().equals("Wrong")){
+				sc = new Scanner(System.in);
+				System.out.println("Gib eine Nummer ein<1,2,3>:");
+				input = sc.nextInt();
+				outStream.println(input);
+			}
 			
-		    String answer = outStream.readLine();
+		    String answer = inStream.readLine();
 		    
-		    System.out.print(answer);
+		    System.out.println(answer);
 		    
+		    sc.close();
 		    client.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		    System.out.println("Client Disconnected!");
+		} catch (UnknownHostException hostEx) {
+			hostEx.printStackTrace();
+		} catch (IOException IOEx) {
+			System.err.println("No Connection");
 		}
 	}
 }
--- src/at/domsi/Server.java
+++ src/at/domsi/Server.java
@@ -1,34 +1,120 @@
 package at.domsi;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Server {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws UnknownHostException {
+		
+		ServerSocket socket=null;
+		
 		try {
-			ServerSocket socket = new ServerSocket(9812);
+			socket = new ServerSocket(9812);
 			System.out.println("ServerSocket Created!");
 			
-			Socket client = socket.accept();
-			System.out.println("Client Connected!");
+			Queue<Player> q = new LinkedList<>();
 			
-			BufferedReader inStream =
-		            new BufferedReader(new InputStreamReader(client.getInputStream()));
-			
-			String in=inStream.readLine();
-			
-			PrintWriter outStream =
-                    new PrintWriter(client.getOutputStream(), true);
-			
-			outStream.println(in);
-			
-			socket.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
+			while(true){
+				Socket client = socket.accept();
+				Player p=new Player(client);
+				q.add(p);
+				System.out.println("Client "+q.size()+" Connected!");
+				
+				p.getOutStream().println("Connected: Player "+q.size());
+				
+				if(q.size()%2==0){
+					Match m=new Match(q.poll(), q.poll());
+					
+					m.getP1().getOutStream().println("GO");
+					m.getP2().getOutStream().println("GO");
+					
+					String inP1;
+					String inP2;
+					int moveP1;
+					int moveP2;
+					
+					boolean inputP1;
+					do{
+						inP1=m.getP1().getInStream().readLine();
+						moveP1=Integer.parseInt(inP1);
+						inputP1=checkP1(moveP1);
+						
+						if(inputP1==false){
+							m.getP1().getOutStream().println("Wrong");
+						}
+					}while(inputP1==false);
+					m.getP1().getOutStream().println("Ok");
+					
+					boolean inputP2;
+					do{
+						inP2=m.getP2().getInStream().readLine();
+						moveP2=Integer.parseInt(inP2);
+						inputP2=checkP2(moveP2);
+						
+						if(inputP2==false){
+							m.getP2().getOutStream().println("Wrong");
+						}
+					}while(inputP2==false);
+					m.getP2().getOutStream().println("Ok");
+					
+					int winner=checkWinner(moveP1, moveP2);
+					
+					m.getP1().getOutStream().println("The Winner is Player "+winner);
+					m.getP2().getOutStream().println("The Winner is Player "+winner);
+				}else{
+					p.getOutStream().println("Wait");
+				}
+			}
+		} catch (IOException IOEx) {
+			IOEx.printStackTrace();
 		}
+		finally{
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static boolean checkP2(int moveP2) {
+		if(moveP2>=0&&moveP2<=3){
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean checkP1(int moveP1) {
+		if(moveP1>=0&&moveP1<=3){
+			return true;
+		}
+		return false;
+	}
+
+	private static int checkWinner(int moveP1, int moveP2) {
+		int winner=1;
+		
+		switch(moveP1){
+			case 1:
+				if(moveP2==2){
+					winner=2;
+				}
+				break;
+			case 2:
+				if(moveP2==3){
+					winner=2;
+				}
+				break;
+			case 3:
+				if(moveP2==1){
+					winner=2;
+				}
+				break;
+		}
+		return winner;
 	}
 }
