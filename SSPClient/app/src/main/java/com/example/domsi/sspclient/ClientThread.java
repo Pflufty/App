f001aa98b6c9ebd7796a9604aeffc05e4b49868b
Kirchsteiger Dominik <domsi198@gmail.com>
2016-06-21 22:49:25 +0200

    3.2.1 SSP Client /w Thread (funktioniert nicht)

--- SSPClient/app/src/main/java/com/example/domsi/sspclient/ClientThread.java
+++ SSPClient/app/src/main/java/com/example/domsi/sspclient/ClientThread.java
@@ -1,5 +1,6 @@
 package com.example.domsi.sspclient;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -7,24 +8,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by User on 21.06.2016.
  */
-public class ClientThread implements Runnable{
+public class ClientThread extends AsyncTask<Void, Void, Void> {
+
     @Override
-    public void run() {
+    protected Void doInBackground(Void... params) {
         try {
-            Log.d("HIT", "Hey");
+            Socket client = new Socket("localhost", 9871);
 
-            MainActivity.client = new Socket("127.0.0.1", 9871);
+            PrintWriter outStream = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader inStream = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            Log.d("HIT", MainActivity.client+"");
+            System.out.println(inStream.readLine());
 
-            MainActivity.inStream= new BufferedReader(new InputStreamReader(MainActivity.client.getInputStream()));
-            MainActivity.outStream= new PrintWriter(MainActivity.client.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
+            while (inStream.readLine().equals("Queue/Wait")) {
+
+            }
+
+            String inputFromServer;
+
+            do {
+
+                outStream.println(MainActivity.move);
+
+                String answer = inStream.readLine();
+
+                int winner = 0;
+
+                if (answer.startsWith("Match/Winner/")) {
+                    int index = answer.lastIndexOf("/");
+                    answer = answer.substring(index + 1, answer.length());
+
+                    String[] win = answer.split(":");
+                    String[] points = win[1].split(";");
+
+                    winner = Integer.parseInt(win[0]);
+
+                    switch (winner) {
+                        case 0:
+                            System.out.println("Unentschieden");
+                            break;
+                        case 1:
+                            System.out.println("Spieler 1 hat gewonnen!");
+                            break;
+                        case 2:
+                            System.out.println("Spieler 2 hat gewonnen!");
+                            break;
+                    }
+
+                    System.out.println(points[0] + " : " + points[1]);
+                }
+
+                inputFromServer = inStream.readLine();
+            } while (inputFromServer.equals("Match/Next"));
+
+            int index = inputFromServer.lastIndexOf("/");
+            index++;
+            inputFromServer = inputFromServer.substring(index, inputFromServer.length());
+            System.out.println();
+            System.out.println("Spieler " + inputFromServer + " hat die Partie gewonnen!");
+
+            client.close();
+        } catch (UnknownHostException hostEx) {
+            hostEx.printStackTrace();
+        } catch (IOException IOEx) {
+            System.err.println("No Connection");
         }
+        return null;
     }
 }
--- SSPClient/app/src/main/java/com/example/domsi/sspclient/MainActivity.java
+++ SSPClient/app/src/main/java/com/example/domsi/sspclient/MainActivity.java
@@ -28,42 +28,98 @@ public class MainActivity extends AppCompatActivity {
     static Button btnStein;
     static Button btnPapier;
 
-    int move=0;
-    boolean selected=false;
-
-    static Socket client=null;
-    static PrintWriter outStream;
-    static BufferedReader inStream;
+    static int move = 0;
+    boolean selected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtP1= (TextView) findViewById(R.id.txtP1);
-        txtP2= (TextView) findViewById(R.id.txtP2);
-        txtScoreP1= (TextView) findViewById(R.id.txtScore1);
-        txtScoreP2= (TextView) findViewById(R.id.txtScore2);
-        txtWinner= (TextView) findViewById(R.id.txtWinner);
-        imgP1= (ImageView) findViewById(R.id.imgP1);
-        imgP2= (ImageView) findViewById(R.id.imgP2);
-        btnSchere= (Button) findViewById(R.id.btnSchere);
-        btnStein= (Button) findViewById(R.id.btnStein);
-        btnPapier= (Button) findViewById(R.id.btnPapier);
+        txtP1 = (TextView) findViewById(R.id.txtP1);
+        txtP2 = (TextView) findViewById(R.id.txtP2);
+        txtScoreP1 = (TextView) findViewById(R.id.txtScore1);
+        txtScoreP2 = (TextView) findViewById(R.id.txtScore2);
+        txtWinner = (TextView) findViewById(R.id.txtWinner);
+        imgP1 = (ImageView) findViewById(R.id.imgP1);
+        imgP2 = (ImageView) findViewById(R.id.imgP2);
+        btnSchere = (Button) findViewById(R.id.btnSchere);
+        btnStein = (Button) findViewById(R.id.btnStein);
+        btnPapier = (Button) findViewById(R.id.btnPapier);
 
-        btnPapier.setClickable(false);
-        btnSchere.setClickable(false);
-        btnStein.setClickable(false);
+        setNotClickable();
 
-        Thread t=new Thread(new ClientThread());
-        t.start();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Socket client = null;
+                try {
+                    client = new Socket("127.0.0.1", 9871);
 
-        while(t.isAlive()){
+                    PrintWriter outStream = new PrintWriter(client.getOutputStream(), true);
+                    BufferedReader inStream = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
+                    System.out.println(inStream.readLine());
+
+                    while (inStream.readLine().equals("Queue/Wait")) {
+
+                    }
+
+                    String inputFromServer;
+
+                    do {
+
+                        outStream.println(MainActivity.move);
+
+                        String answer = inStream.readLine();
+
+                        int winner = 0;
+
+                        if (answer.startsWith("Match/Winner/")) {
+                            int index = answer.lastIndexOf("/");
+                            answer = answer.substring(index + 1, answer.length());
+
+                            String[] win = answer.split(":");
+                            String[] points = win[1].split(";");
+
+                            winner = Integer.parseInt(win[0]);
+
+                            switch (winner) {
+                                case 0:
+                                    System.out.println("Unentschieden");
+                                    break;
+                                case 1:
+                                    System.out.println("Spieler 1 hat gewonnen!");
+                                    break;
+                                case 2:
+                                    System.out.println("Spieler 2 hat gewonnen!");
+                                    break;
+                            }
+
+                            System.out.println(points[0] + " : " + points[1]);
+                        }
+
+                        inputFromServer = inStream.readLine();
+                    } while (inputFromServer.equals("Match/Next"));
+
+                    int index = inputFromServer.lastIndexOf("/");
+                    index++;
+                    inputFromServer = inputFromServer.substring(index, inputFromServer.length());
+                    System.out.println();
+                    System.out.println("Spieler " + inputFromServer + " hat die Partie gewonnen!");
+
+                    client.close();
+                } catch (UnknownHostException hostEx) {
+                    hostEx.printStackTrace();
+                } catch (IOException IOEx) {
+                    System.err.println("No Connection");
+                }
+            }
+
+        });
+
+            //new ClientThread().execute();
         }
-
-
-    }
 
     public static void startGame() {
         btnPapier.setClickable(true);
@@ -71,18 +127,27 @@ public class MainActivity extends AppCompatActivity {
         btnStein.setClickable(true);
     }
 
+    public static void setNotClickable() {
+        btnPapier.setClickable(false);
+        btnSchere.setClickable(false);
+        btnStein.setClickable(false);
+    }
+
     public void btnSchereClicked(View view) {
-        move=1;
-        selected=true;
+        move = 1;
+        selected = true;
+        setNotClickable();
     }
 
     public void btnSteinClicked(View view) {
-        move=2;
-        selected=true;
+        move = 2;
+        selected = true;
+        setNotClickable();
     }
 
     public void btnPapierClicked(View view) {
-        move=3;
-        selected=true;
+        move = 3;
+        selected = true;
+        setNotClickable();
     }
 }
